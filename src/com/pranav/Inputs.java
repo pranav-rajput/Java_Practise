d8df8f0fcad6082a0499be82e1af058074f9b741
Pranav <90679961+pranav-rajput@users.noreply.github.com>
2024-04-25 01:13:00 +0530

    third commit

---
+++ src/com/pranav/Inputs.java
@@ -0,0 +1,14 @@
+package com.pranav;
+
+import java.util.Scanner;
+
+public class Inputs {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter roll no:");
+        int roll_no = input.nextInt();
+        System.out.print("the roll no is:"+ roll_no);
+
+    }
+}
