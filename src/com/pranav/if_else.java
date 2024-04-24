91d560958b5e049a927d50c5739fad408ff492bf
Pranav <90679961+pranav-rajput@users.noreply.github.com>
2024-04-25 01:08:46 +0530

    second commit

---
+++ src/com/pranav/if_else.java
@@ -0,0 +1,21 @@
+package com.pranav;
+
+import java.util.Scanner;
+
+public class if_else {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("enter salary: ");
+        int salary = in.nextInt();
+        if(salary > 10000 && salary < 15000){
+            salary = salary + 2000;
+        } else if (salary > 15000) {
+            salary += 1500;
+
+        } else{
+            salary = salary +1000;
+        }
+        System.out.println(salary);
+    }
+
+}
