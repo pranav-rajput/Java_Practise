3e295868f1730aec5e05aa0bb4d35a42a0c6b5b6
Pranav <90679961+pranav-rajput@users.noreply.github.com>
2024-04-28 22:02:06 +0530

    commit

---
+++ src/com/pranav/Primitives.java
@@ -0,0 +1,14 @@
+package com.pranav;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int roll0no = 17;
+        char name = 'p';
+        float no = 12.45f;
+        double nos = 12.4661610;
+        long integers = 57546464616463L;
+        boolean check = true;
+
+
+    }
+}
